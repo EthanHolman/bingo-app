7f5d19525b62f47b8a65e7c57734c0adc1350897
Ethan Holman <96ethanh@gmail.com>
2024-11-03 15:55:43 -0800

    Adding post category endpoint

--- api/endpoints/post_category.py
+++ api/endpoints/post_category.py
@@ -1,5 +1,17 @@
-# import boto3
+import boto3
+import json
 
-# dynamo = boto3.resource("dynamodb")
 
-# table = dynamo.Table("bingo-app-default-datastore")
+def handler(event, context):
+    print(event)
+    body = json.loads(event.get("body", "{}"))
+    print(body)
+
+    new_category = body.get("category", None)
+
+    if new_category:
+        dynamo = boto3.resource("dynamodb")
+
+        table = dynamo.Table("bingo-app-default-datastore")
+
+        table.put_item(Item={"PK": "category", "SK": new_category})
--- terraform/api.tf
+++ terraform/api.tf
@@ -3,12 +3,16 @@ module "api" {
 
   resource_prefix     = local.resource_prefix
   lambda_policy_names = var.lambda_policy_names
-  api_src_path        = "${path.module}/../api/"
+  api_src_path        = "${path.module}/../api/venv"
 
   api_endpoints = {
     "get_category" = {
       route    = "GET /category",
       filename = "${path.module}/../api/endpoints/get_category.py"
+    },
+    "post_category" = {
+      route    = "POST /category",
+      filename = "${path.module}/../api/endpoints/post_category.py"
     }
   }
 }
